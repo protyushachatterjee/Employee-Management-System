831236966b570bcd6e014647627c1869c8fc0f18
protyushachatterjee <protyushachatterjee940@gmail.com>
2024-11-14 23:35:36 +0530

    end commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/employee-management-system/',
   plugins: [react()],
+  base: '/employee-management-system/',
 })
